5bcc11b6869ac7e04cc3d074e89f60c392292bd7
Patricia Carter <patricia4194@gmail.com>
2018-08-08 02:13:45 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,7 +6,7 @@ def my_select(collection)
     i = 0
     
     while i < collection.length
-      if yield collection[i] == true
+      if yield(collection[i]) == true
         new_array << collection[i]
       end 
       i += 1
